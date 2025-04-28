66268ea458882a2944a2eb44cd50ca51bda0058e
Nukumizu05 <yangliu2@ufl.edu>
2025-04-28 17:43:53 -0400

    Wrapping WelcomeWindow into a class

--- main.cpp
+++ main.cpp
@@ -1,102 +1,170 @@
-
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <unordered_map>
+#include <random>
+#include <cmath>
 
-std::string formatName(const std::string& input) {
-    std::string formatted = input;
-    formatted[0] = std::toupper(formatted[0]);
-    for (size_t i = 1; i < formatted.size(); ++i) {
-        formatted[i] = std::tolower(formatted[i]);
-    }
-    return formatted;
+static void setText(sf::Text& text, float x, float y)
+{
+    sf::FloatRect textRect = text.getLocalBounds();
+    text.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+    text.setPosition(sf::Vector2f(x, y));
 }
 
-int main() {
-
-    int welcomeWindowWidth = 800;
-    int welcomeWindowHeight = 600;
-
-    sf::RenderWindow welcomeWindow(sf::VideoMode(welcomeWindowWidth, welcomeWindowHeight), "Welcome", sf::Style::Close);
-
+class WelcomeWindow
+{
+private:
+    unsigned int width;
+    unsigned int height;
     sf::Font font;
-    font.loadFromFile("../files/font.ttf");
 
-    sf::Text welcomeText("Welcome to (game_name)!", font, 24);
-    welcomeText.setPosition((welcomeWindow.getSize().x / 2) - (welcomeText.getLocalBounds().width / 2), welcomeWindow.getSize().y / 2 - 200);
-    welcomeText.setFillColor(sf::Color::White);
-    welcomeText.setStyle(sf::Text::Underlined | sf::Text::Bold);
+public:
+    WelcomeWindow()
+    {
+        width = 800;
+        height = 600;
 
-
-    sf::Text promptText("Please enter the name of your tycoon:", font, 20);
-    promptText.setPosition((welcomeWindowWidth / 2) - (promptText.getLocalBounds().width / 2), (welcomeWindowHeight / 2) - 75);
-    promptText.setFillColor(sf::Color::White);
-    promptText.setStyle(sf::Text::Bold);
-
-    sf::Text userInput("|", font, 18);
-    userInput.setPosition((welcomeWindowWidth / 2) - (userInput.getLocalBounds().width / 2), (welcomeWindowHeight / 2) - 45);
-    userInput.setFillColor(sf::Color::White);
-    std::string inputText = "";
-    std::string tycoonName = "";
-
-    sf::Sprite palmTree;
-    sf::Sprite money;
-    sf::Sprite ferrisWheel;
-
-    sf::Texture palmTreeTexture;
-    sf::Texture moneyTexture;
-    sf::Texture ferrisWheelTexture;
-
-    palmTreeTexture.loadFromFile("../files/palmtree.png");
-    palmTree.setTexture(palmTreeTexture);
-    palmTree.setPosition(sf::Vector2f(50, 50));
-    palmTree.setScale(.25f, .25f);
-
-    moneyTexture.loadFromFile("../files/moneymoney.png");
-    money.setTexture(moneyTexture);
-    money.setPosition(sf::Vector2f(640, 70));
-    money.setScale(.05f, .05f);
-
-    ferrisWheelTexture.loadFromFile("../files/ferriswheel.png");
-    ferrisWheel.setTexture(ferrisWheelTexture);
-    ferrisWheel.setPosition(sf::Vector2f(300, 300));
-    ferrisWheel.setScale(.4f, .4f);
-
-    while(welcomeWindow.isOpen()) {
-        sf::Event event;
-        while(welcomeWindow.pollEvent(event)) {
-            if(event.type == sf::Event::Closed) {
-                welcomeWindow.close();
-                exit(0);
-            }
-            if (event.type == sf::Event::TextEntered) {
-                if (event.text.unicode == 8) {
-                    if (!inputText.empty()) {
-                        inputText.pop_back();
-                    }
-                } else if ((std::isalpha(event.text.unicode) || event.text.unicode == ' ' )&& inputText.size() < 20) {
-                    inputText += static_cast<char>(event.text.unicode);
-                }
-                userInput.setString(formatName(inputText) + "|");
-                userInput.setPosition((welcomeWindowWidth / 2) - (userInput.getLocalBounds().width / 2), (welcomeWindowHeight / 2) - 45);
-            }
-            if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Enter)) {
-                if (!inputText.empty()) {
-                    tycoonName = inputText;
-                    welcomeWindow.close();
-                }
-            }
+        //Load Font
+        if (!font.loadFromFile("font.ttf"))
+        {
+            std::cerr << "Failed to open font file!" << std::endl;
         }
-
-
-        welcomeWindow.clear(sf::Color(10, 172, 96));
-        welcomeWindow.draw(welcomeText);
-        welcomeWindow.draw(promptText);
-        welcomeWindow.draw(userInput);
-        welcomeWindow.draw(palmTree);
-        welcomeWindow.draw(money);
-        welcomeWindow.draw(ferrisWheel);
-        welcomeWindow.display();
     }
 
+    void launchWelcomeWindow()
+    {
+        //Create Welcome Window
+        sf::RenderWindow welcomeWindow(sf::VideoMode({ width, height }), "Welcome", sf::Style::Default);
+        welcomeWindow.setFramerateLimit(60);
 
-  return 0;
+        //Set Background
+        sf::RectangleShape background(sf::Vector2f(width, height));
+        background.setFillColor(sf::Color::Blue);
+
+        sf::Text welcomeText("Welcome to (game_name)!", font, 24);
+        welcomeText.setPosition((welcomeWindow.getSize().x / 2) - (welcomeText.getLocalBounds().width / 2), welcomeWindow.getSize().y / 2 - 200);
+        welcomeText.setFillColor(sf::Color::White);
+        welcomeText.setStyle(sf::Text::Underlined | sf::Text::Bold);
+
+
+        sf::Text promptText("Please enter the name of your tycoon:", font, 20);
+        promptText.setPosition((width / 2) - (promptText.getLocalBounds().width / 2), (height / 2) - 75);
+        promptText.setFillColor(sf::Color::White);
+        promptText.setStyle(sf::Text::Bold);
+
+        std::string playerName;
+        sf::Text inputText("", font, 18);
+        inputText.setStyle(sf::Text::Bold);
+        inputText.setFillColor(sf::Color::White);
+        setText(inputText, width / 2.0f, height / 2.0f - 25);
+
+        sf::Sprite palmTree;
+        sf::Sprite money;
+        sf::Sprite ferrisWheel;
+
+        sf::Texture palmTreeTexture;
+        sf::Texture moneyTexture;
+        sf::Texture ferrisWheelTexture;
+
+        palmTreeTexture.loadFromFile("images/palmtree.png");
+        palmTree.setTexture(palmTreeTexture);
+        palmTree.setPosition(sf::Vector2f(50, 50));
+        palmTree.setScale(.25f, .25f);
+
+        moneyTexture.loadFromFile("images/moneymoney.png");
+        money.setTexture(moneyTexture);
+        money.setPosition(sf::Vector2f(640, 70));
+        money.setScale(.05f, .05f);
+
+        ferrisWheelTexture.loadFromFile("images/ferriswheel.png");
+        ferrisWheel.setTexture(ferrisWheelTexture);
+        ferrisWheel.setPosition(sf::Vector2f(300, 300));
+        ferrisWheel.setScale(.4f, .4f);
+
+        sf::Text cursor("|", font, 20);
+        cursor.setFillColor(sf::Color::Yellow);
+        bool showCursor = true;
+
+        while (welcomeWindow.isOpen()) {
+            sf::Event event;
+            while (welcomeWindow.pollEvent(event)) {
+                if (event.type == sf::Event::Closed) {
+                    welcomeWindow.close();
+                    exit(0);
+                }
+                if (event.type == sf::Event::TextEntered)
+                {
+                    //Backspace
+                    if (event.text.unicode == '\b')
+                    {
+                        if (!playerName.empty())
+                        {
+                            playerName.pop_back();
+                        }
+                    }
+                    //Alphabet Check
+                    else if (isalpha(static_cast<char>(event.text.unicode)) && playerName.size() < 10)
+                    {
+                        char input = static_cast<char>(event.text.unicode);
+
+                        if (playerName.empty())
+                        {
+                            playerName += toupper(input);
+                        }
+                        else
+                        {
+                            playerName += tolower(input);
+                        }
+                    }
+
+                    inputText.setString(playerName);
+                    setText(inputText, width / 2.0f, height / 2.0f - 25);
+                }
+                if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Enter)) {
+                    if (!playerName.empty())
+                    {
+                        welcomeWindow.close();
+                        //GameWindow Opens
+                    }
+                }
+                if (!playerName.empty())
+                {
+                    sf::FloatRect bounds = inputText.getLocalBounds();
+                    cursor.setPosition(inputText.getPosition().x + bounds.width / 2.0f + 2, inputText.getPosition().y);
+                }
+                else
+                {
+                    setText(cursor, width / 2.0f, inputText.getPosition().y);
+                }
+            }
+
+
+            welcomeWindow.clear();
+            welcomeWindow.draw(background);
+            welcomeWindow.draw(welcomeText);
+            welcomeWindow.draw(promptText);
+            welcomeWindow.draw(inputText);
+            welcomeWindow.draw(palmTree);
+            welcomeWindow.draw(money);
+            welcomeWindow.draw(ferrisWheel);
+            if (showCursor)
+            {
+                welcomeWindow.draw(cursor);
+            }
+            welcomeWindow.display();
+        }
+    }
+};
+
+int main()
+{
+    WelcomeWindow ww;
+
+    ww.launchWelcomeWindow();
+
+    return 0;
 }
