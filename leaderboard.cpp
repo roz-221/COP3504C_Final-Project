5e9c11aacc30284002ef982da47012322eb65c82
MonishB123 <monishbeegamudre@gmail.com>
2025-04-28 20:59:41 -0400

    applied bubble sort on player list to sort

--- leaderboard.cpp
+++ leaderboard.cpp
@@ -32,6 +32,16 @@ struct Leaderboard{
                 players.push_back(Player(time, name));
             }
         }
+        int n = players.size();
+        for(int i = 0; i < n - 1; i++){
+            for (int j = 0; j < n - i - 1; j++){
+                if (players.at(j).getTime() > players.at(j+1).getTime()){
+                    Player temp = players[j];
+                    players[j] = players[j+1];
+                    players[j+1] = temp;
+                }
+            }
+        }
     }
 
     void launchLeaderboard(){
