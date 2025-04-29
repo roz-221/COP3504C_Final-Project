d0538c4934b54955a50e0af264f983f2448cc809
Nukumizu05 <yangliu2@ufl.edu>
2025-04-28 20:14:58 -0400

    Add class Building and GameWindow

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include <unordered_map>
 #include <random>
 #include <cmath>
-#include "building.h"
+#include <vector>
 
 static void setText(sf::Text& text, float x, float y)
 {
@@ -55,7 +55,148 @@ public:
     }
 };
 
+class Building {
+    std::string name;
+    int building_count;
+    int building_price;
+    sf::Sprite building_sprite;
+    sf::Texture building_texture
+        int earn_rate;
+    bool unlocked;
 
+public:
+    Building() : building_count(0), building_price(0), earn_rate(0), unlocked(false) {}
+
+    Building(int building_count, int building_price, int earn_rate, sf::Sprite sprite) {
+        this->building_count = building_count;
+        this->building_price = building_price;
+        this->earn_rate = earn_rate;
+        this->building_sprite = sprite;
+    }
+
+    int purchaseBuilding() {
+        int original_price = building_price;
+        building_count += 1;
+        building_price *= 1.08;
+        return original_price;
+    }
+
+    int getEarnRate() {
+        return earn_rate;
+    }
+    int getBuildingCount() {
+        return building_count;
+    }
+    int getBuildingPrice() {
+        return building_price;
+    }
+    int getUnlocked() {
+        return unlocked;
+    }
+    std::string getName() {
+        return name;
+    }
+    sf::Sprite getBuildingSprite() {
+        return building_sprite;
+    }
+    void setEarnRate(int rate) {
+        earn_rate = rate;
+    }
+    void setBuildingCount(int count) {
+        building_count = count;
+    }
+    void setBuildingPrice(int price) {
+        building_price = price;
+    }
+    void setBuildingSprite(sf::Sprite sprite) {
+        building_sprite = sprite;
+    }
+
+    void setBuildingTexture()
+    {
+        if (!building_texture.loadFromFile("images/" + name + ".png"))
+        {
+            std::cerr << "Failed to load hidden tile!" << std::endl;
+            return false;
+        }
+        building_sprite.setTexture(building_texture);
+        building_sprite.setTextureRect(sf::IntRect(0, 0, 200, 200))
+    }
+};
+
+class GameWindow {
+    float balance;
+    sf::Sprite avatar; // Winning & Normal
+
+    // Buttons
+    sf::Sprite pause;
+    sf::Sprite reset;
+    sf::Sprite leaderboard;
+    sf::Sprite upgrade;
+
+    std::vector<Building> buildings;
+    int height;
+    int width;
+    bool paused;
+    bool reset;
+
+public:
+    GameWindow() : balance(50), width(1500), height(1200), {}
+        void loadTextures() {
+        for (size_t i = 0; i < buildings.size(); ++i) {
+            std::string filename = "images/";
+            filename += buildings.at(i).getName();
+            buildings.at(i).setTexture(filename);
+        }
+    }
+    void togglePause() {
+        if (paused) {
+
+        }
+    }
+    void toggleReset() {
+        // Set everything to default
+        if (reset) {
+
+        }
+    }
+    void toggleLeaderboard() {
+        // load leaderboard
+    }
+    void handleClick(int x, int y, sf::Mouse::button button) {
+        // IDK
+    }
+    void run() {
+        sf::Font font;
+        if (!font.loadFromFile("font.ttf")) {
+            std::cout << "Failed to read font file." << std::endl;
+            return;
+        }
+
+        sf::RenderWindow gameWindow(sf::VideoMode({ width, height }), "Game Window");
+        sf::Text title(font, "Testing's Tycoon", 30);
+
+        while (gameWindow.isOpen()) {
+            sf::Event event;
+            while (gameWindow.pollEvent(event)) {
+                if (event.type == sf::Event::Closed) {
+                    gameWindow.close();
+                }
+                if (event.type == sf::Event::MouseButtonPressed) {
+                    handleClick(event.mouseButton.x, event.mouseButton.y, event.mouseButton.button);
+                }
+            }
+            // Change to money picture
+            gameWindow.clear(sf::Color::White);
+
+            gameWindow.draw(title);
+            for (size_t i = 0; i < buildings.size(); ++i) {
+                gameWindow.draw(buildings.at(i).getBuildingSprite());
+            }
+
+        }
+    }
+}
 
 class WelcomeWindow
 {
